8ba16bef6937ad27311a55b74f42e167ee012219
corran2go <corran2go@gmail.com>
2010-10-26 11:24:10 +0200

    added viewports, removed unneeded stuff

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,12 @@
 (function playground() {
+  var screenNode = document.getElementById("screenResolution");
+  var lviewportNode = document.getElementById("lviewportResolution");
+  var vviewportNode = document.getElementById("vviewportResolution");
+
   function displayResolutions() {
-    var screenNode = document.getElementById("screenResolution");
-    var availNode = document.getElementById("availableResolution");
-    var windowNode = document.getElementById("windowResolution");
     screenNode.textContent = "screen: " + screen.width + "x" + screen.height;
-    availNode.textContent = "avail: " + screen.availWidth + "x" + screen.availHeight;
-    windowNode.textContent = "window: " + window.innerWidth + "x" + window.innerHeight;
+    lviewportNode.textContent = "layout viewport: " + document.documentElement.clientWidth + "x" + document.documentElement.clientHeight;
+    vviewportNode.textContent = "visual viewport: " + window.innerWidth + "x" + window.innerHeight;
   }
 
   addEventListener("resize", function (ev) {
