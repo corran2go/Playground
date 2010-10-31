3a7723f0b431763a0efde981ec3369a66026fe31
corran2go <corran2go@gmail.com>
2010-10-31 22:26:28 +0100

    canvas plays

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,21 @@
 (function playground() {
-  var screenNode = document.getElementById("screenResolution");
-  var lviewportNode = document.getElementById("lviewportResolution");
-  var vviewportNode = document.getElementById("vviewportResolution");
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext("2d");
+  var canvasData = context.createImageData(canvas.width, canvas.height);
 
-  function displayResolutions() {
-    screenNode.textContent = "screen: " + screen.width + "x" + screen.height;
-    lviewportNode.textContent = "layout viewport: " + document.documentElement.clientWidth + "x" + document.documentElement.clientHeight;
-    vviewportNode.textContent = "visual viewport: " + window.innerWidth + "x" + window.innerHeight;
+  function setPixel(imageData, x, y, r, g, b, a) {
+    var index = (x + y * imageData.width) * 4;
+    imageData.data[index+0] = r;
+    imageData.data[index+1] = g;
+    imageData.data[index+2] = b;
+    imageData.data[index+3] = a;
   }
 
-  addEventListener("resize", function (ev) {
-    displayResolutions();
-  }, false );
-
-  displayResolutions();
+  for (var x = 0; x < canvasData.width; x++) {
+    for (var y = 0; y < canvasData.height; y++) {
+	  setPixel(canvasData, x, y, parseInt(Math.random() * 256), parseInt(Math.random() * 256), parseInt(Math.random() * 256), 0xff);
+	  //console.log("put pixel");
+	}
+  }
+  context.putImageData(canvasData, 0, 0);
 })();
